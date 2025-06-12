5f8c6dfb4dc950c3cebb317bab76c13008785a38
Amanda F Pereira <amanda.fernandes2030@gmail.com>
2025-06-12 14:11:07 -0300

    Update Cartão

--- api/src/main/java/com/ibmec/mall/controller/CartaoCreditoController.java
+++ api/src/main/java/com/ibmec/mall/controller/CartaoCreditoController.java
@@ -4,12 +4,17 @@ import com.ibmec.mall.model.CartaoCredito;
 import com.ibmec.mall.model.Usuario;
 import com.ibmec.mall.repository.CartaoCreditoRepository;
 import com.ibmec.mall.repository.UsuarioRepository;
+import com.ibmec.mall.request.TransacaoRequest;
+import com.ibmec.mall.request.TransacaoResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/cartoes/{id_user}")
@@ -47,5 +52,69 @@ public class CartaoCreditoController {
 
         Usuario usuario = optionalUsuario.get();
         return new ResponseEntity<>(usuario.getCartoes(), HttpStatus.OK);
+    }
+
+     @PostMapping("/authorize")
+    public ResponseEntity<TransacaoResponse> authorize(@PathVariable("id_user") int id_user,
+                                                       @RequestBody TransacaoRequest request) {
+        Optional<Usuario> optionalUsuario = this.usuarioRepository.findById(id_user);
+
+        if (optionalUsuario.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        if (request.getNumero() == null || request.getCvv() == null || request.getValor() == null) {
+            TransacaoResponse response = new TransacaoResponse();
+            response.setStatus("NOT_AUTHORIZED");
+            response.setData(LocalDateTime.now());
+            response.setMensagem("Dados da transação incompletos");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        Usuario user = optionalUsuario.get();
+        CartaoCredito cartaoTransacao = null;
+
+        for (CartaoCredito cartao: user.getCartoes()) {
+            if (cartao.getNumero() != null && cartao.getCvv() != null &&
+                cartao.getNumero().equals(request.getNumero()) && cartao.getCvv().equals(request.getCvv())) {
+                cartaoTransacao = cartao;
+                break;
+            }
+        }
+
+        if (cartaoTransacao == null) {
+            TransacaoResponse response = new TransacaoResponse();
+            response.setStatus("NOT_AUTHORIZED");
+            response.setData(LocalDateTime.now());
+            response.setMensagem("Cartão não encontrado para o usuario");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+
+        if (cartaoTransacao.getDtExpiracao() == null || cartaoTransacao.getDtExpiracao().isBefore(LocalDateTime.now())) {
+            TransacaoResponse response = new TransacaoResponse();
+            response.setStatus("NOT_AUTHORIZED");
+            response.setData(LocalDateTime.now());
+            response.setMensagem("Cartão Expirado");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        if (cartaoTransacao.getSaldo() == null || cartaoTransacao.getSaldo() < request.getValor()) {
+            TransacaoResponse response = new TransacaoResponse();
+            response.setStatus("NOT_AUTHORIZED");
+            response.setData(LocalDateTime.now());
+            response.setMensagem("Sem saldo para realizar a compra");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        Double saldo = cartaoTransacao.getSaldo() - request.getValor();
+        cartaoTransacao.setSaldo(saldo);
+        this.cartaoCreditoRepository.save(cartaoTransacao);
+
+        TransacaoResponse response = new TransacaoResponse();
+        response.setStatus("AUTHORIZED");
+        response.setData(LocalDateTime.now());
+        response.setMensagem("Compra autorizada");
+        response.setCodigoAutorizacao(UUID.randomUUID().toString());
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
